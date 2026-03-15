dc136f5dfda51f3cbfe371a06933570a5f4bffab
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2026-03-15 13:42:09 +0100

    chore: add linkbreakers package init file

---
+++ linkbreakers/__init__.py
@@ -0,0 +1,14 @@
+# Linkbreakers Python SDK
+# Auto-generated from OpenAPI specification
+
+__version__ = '1.32.0'
+
+# Re-export generated API classes (will be populated after generation)
+try:
+    from linkbreakers.api import *
+    from linkbreakers.models import *
+    from linkbreakers.configuration import Configuration
+    from linkbreakers.api_client import ApiClient
+except ImportError:
+    # Not yet generated
+    pass
